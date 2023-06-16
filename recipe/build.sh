58ec7ee04c29ebedce11d473732dcb8bf338cc4b
Mervin Fansler <mmfansler@gmail.com>
2023-06-16 03:04:37 -0400

    tolerate older osx sdk

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# inject makevars via user config
+mkdir -p ~/.R && echo "CXXFLAGS = -D_LIBCPP_DISABLE_AVAILABILITY" >> ~/.R/Makevars
+
 export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
